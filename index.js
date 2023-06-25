afc5fffa7c505fbac3c4be78df7e0b1742d1d3d9
Tobithedev <showgame360@gmail.com>
2023-06-25 18:26:23 +0100

    feat: assignment fixed

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const items = document.querySelectorAll(".accordion button");
+const $items = document.querySelectorAll(".accordion button");
 const icon = document.querySelector(".fa-plus");
 
-function toggleAccordion() {
-  const itemToggle = this.getAttribute("aria-expanded");
+function toggle() {
+  const item = this.getAttribute("aria-expanded");
 
-  for (i = 0; i < items.length; i++) {
-    items[i].setAttribute("aria-expanded", "false");
+  for (i = 0; i < $items.length; i++) {
+    $items[i].setAttribute("aria-expanded", "false");
   }
 
-  if (itemToggle == "false") {
+  if (item == "false") {
     this.setAttribute("aria-expanded", "true");
   }
 }
 
-items.forEach((item) => item.addEventListener("click", toggleAccordion));
+$items.forEach((item) => item.addEventListener("click", toggle));
